8606fa28bfa999f7a3a3e68c8721debc0bdeb9e2
FourSqweez <four7518@gmail.com>
2021-03-14 20:18:50 +0700

    6. Any / Unknown

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 "use strict";
 exports.__esModule = true;
-// Enum (Enumerated type)
-var Role;
-(function (Role) {
-    Role["CLIENT"] = "CLIENT";
-    Role["EDITOR"] = "EDITOR";
-    Role["ADMIN"] = "ADMIN";
-    Role["SUPER_ADMIN"] = "SUPER_ADMIN";
-})(Role || (Role = {}));
-console.log(Role);
+// Any
+var a;
+// a = 1
+// a = 'a'
+// a = true
+// a = [1, 2]
+// a = { a: 1, b: 2 }
+// console.log('a', a)
+// Unknown
+var x;
+x = 'a';
+if (typeof x === 'string') {
+    var y = x.toUpperCase();
+    console.log('y: ', y);
+}
+// x = 'b'
+// x = false
+// x = ['a','b']
+// x = {a: 5, b:7}
+console.log('x: ', x);
--- app.ts
+++ app.ts
@@ -1,12 +1,30 @@
-
 export {}
-// Enum (Enumerated type)
+// Any
 
-enum Role {
-    CLIENT = 'CLIENT',
-    EDITOR = 'EDITOR',
-    ADMIN = 'ADMIN',
-    SUPER_ADMIN = 'SUPER_ADMIN',
+let a: any
+
+a = 1
+
+// a = 'a'
+// a = true
+// a = [1, 2]
+// a = { a: 1, b: 2 }
+// console.log('a', a)
+
+// Unknown
+
+let x: unknown
+
+x = 'a'
+
+if (typeof x === 'string') {
+  const y = x.toUpperCase()
+  console.log('y: ', y)
 }
 
-console.log(Role)
+// x = 'b'
+// x = false
+// x = ['a','b']
+// x = {a: 5, b:7}
+
+console.log('x: ', x)
