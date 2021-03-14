af30f0e912081c1421bb6367a3e9e53853bd93fb
FourSqweez <four7518@gmail.com>
2021-03-14 19:16:47 +0700

    2.2 : type inference

---
+++ reference.ts
@@ -0,0 +1,24 @@
+export {}
+let num: number
+num = 5
+
+let greet1 = 'Hi'
+const greet2 = 'Hello'
+let isEqual1 = true
+const isEqual2 = false
+
+const numbers = [5, 6, 7]
+
+const names = ['tim', 'tom', 'jane']
+
+const user = {
+  name: 'time',
+  email: 'tim@test.com',
+  password: '1234',
+  age: 15,
+  gender: 'Male',
+}
+
+const sum = (x: number, y: number) => {
+  return x + y
+}
