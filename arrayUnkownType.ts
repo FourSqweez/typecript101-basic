a9ac32f08478a02c715842da853a8bdb03a8a86e
FourSqweez <four7518@gmail.com>
2022-07-21 18:29:10 +0700

    Add: unknown type

---
+++ arrayUnkownType.ts
@@ -0,0 +1,12 @@
+// Array type
+// let sports: string[]; -> Error Variable 'sports' is used before being assigned
+
+let sports: string[] = []; // Array<string>
+sports.push('football');
+
+console.log(sports);
+
+const numbers = [1, 2, 3]; // -> const numbers: number[]
+
+
+
