f4979a8e330de20a10acffdf0f0a047773c05d07
FourSqweez <four7518@gmail.com>
2022-07-21 18:34:31 +0700

    Add: config

--- reference.ts
+++ reference.ts
@@ -1,11 +1,14 @@
 export {}
+
+// reference มาจาก annotations.ts
 let num: number
 num = 5
 
-let greet1 = 'Hi'
-const greet2 = 'Hello'
+let greet1 = 'Hi' // เราลบ type ออกได้ เพราะ typescript มันรู้เองว่าเป็นสตริง สามารถเปลี่ยนค่าได้แต่ต้องเป็นสตริง
+const greet2 = 'Hello' // ถ้าเป็น const แล้วเราไม่ได้ระบุ type แต่แรก ตัวนี้จะมี type เป็น Hello ทันที เพราะ const เป็นค่าคงที่
 let isEqual1 = true
-const isEqual2 = false
+const isEqual2 = false 
+
 
 const numbers = [5, 6, 7]
 
