aa041a3a161987184f6f77f5d815b3d9cd5f6a8b
FourSqweez <four7518@gmail.com>
2022-07-21 18:26:23 +0700

    Add: annotation type

--- annotations.ts
+++ annotations.ts
@@ -1,34 +1,41 @@
-export {}
-let num: number
-num = 5
 
-let greet1: string = 'Hi'
+// ทั้งหมดนี้คือการ annotate
 
-const greet2: string = 'Hello'
-let isEqual1: boolean = true
-const isEqual2: boolean = false
+export {};
+let num: number;
+num = 5;
 
-const numbers: number[] = [5, 6, 7]
+let greet1: string = 'Hi';
 
-const names: Array<string> = ['tim', 'tom', 'jane']
+const greet2: string = 'Hello';
+let isEqual1: boolean = true;
+const isEqual2: boolean = false;
 
+const numbers: number[] = [5, 6, 7];
+
+//Array
+const names: Array<string> = ['tim', 'tom', 'jane'];
+
+//Object
 const user: {
-  name?: string
-  email: string
-  password: string
-  age: number
-  gender: 'Male' | 'Female'
+  // ? => มีก็ได้หรือไม่มีก็ได้
+	name?: string;
+	email: string;
+	password: string;
+	age: number;
+	gender: 'Male' | 'Female';
 } = {
-  name: 'time',
-  email: 'tim@test.com',
-  password: '1234',
-  age: 15,
-  gender: 'Male',
-}
+	name: 'time',
+	email: 'tim@test.com',
+	password: '1234',
+	age: 15,
+	gender: 'Male',
+};
 
+// เขียนแบบ annotate แต่จริงๆควรให้ typescript มัน infer เอง เหมือนในไฟล์ reference.ts จะสั้นลง
 const sum: (a: number, b: number) => number = (
-  x: number,
-  y: number
+	x: number, // ลองให้ดูว่าชื่อไม่เหมือนกันได้ แต่ type ต้องเหมือนกัน แต่ปกติก็ไม่ทำแบบนี้
+	y: number
 ): number => {
-  return x + y
-}
+	return x + y;
+};
